d5ebbfe637b6738c101f4e2d01472e5ff3b9ba50
erik <Erik.Varian@contracted.sampoerna.com>
2025-11-30 21:22:43 +0700

    fix formatting

--- serverless-architecture/variables.tf
+++ serverless-architecture/variables.tf
@@ -12,7 +12,7 @@ variable "python_version" {
 variable "project_name" {
 
   default = "microservice-architecture"
-  type = string
+  type    = string
 }
 
 variable "qroq_api" {
@@ -23,10 +23,10 @@ variable "qroq_api" {
 variable "lambda_initial_spec" { //   <--- value ideally stored and taken from terraform enterprise. will make changing lambda spec easier without altering code and just need to change var on tf enterise + apply.
 
   default = {
-    "memory_size"      = 512
-    "timeout"          = 10
+    "memory_size" = 512
+    "timeout"     = 10
     "ephemeral_storage" = {
-      size = 512  
+      size = 512
     }
   }
 }
