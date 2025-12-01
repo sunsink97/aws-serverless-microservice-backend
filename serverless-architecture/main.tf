5e0d4cf7e95aa865289a2596c022b064edd4c6e3
erik <Erik.Varian@contracted.sampoerna.com>
2025-12-01 15:12:16 +0700

    add backend using s3 bucket for 1 tf state

--- serverless-architecture/main.tf
+++ serverless-architecture/main.tf
@@ -5,3 +5,13 @@ locals {
     managedby = "Terraform"
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-demo-serverless-architecture"  
+    key            = "microservice-chat-bot/terraform.tfstate"
+    region         = "us-east-1"                  
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
